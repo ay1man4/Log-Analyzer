05d3ce56c874ab31edeafce3c0ce67838528a341
Ayman Ahmed <absi.ayman@gmail.com>
2018-11-05 22:55:52 +0300

    Ready to use

---
+++ README.md
@@ -0,0 +1,28 @@
+# Log Analyzer
+
+Log Analyzer is a python script to analyze logs of a news blog. It give answers of three questions:
+
+1. What are the most popular three articles of all time?
+1. Who are the most popular article authors of all time?
+1. On which days did more than 1% of requests lead to errors?
+
+## Install
+
+Before using this script, you have to prepare news DB and required softwares to run the Log Analyzer. This illustration showing how to use VM environment using vagrant. Follow below steps to prepare the environment and DB:
+
+- Install [vagrant](https://www.vagrantup.com/downloads.html) and [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+- Download and unzip the VM configuration [FSND-Virtual-Machine.zip](https://s3.amazonaws.com/video.udacity-data.com/topher/2018/April/5acfbfa3_fsnd-virtual-machine/fsnd-virtual-machine.zip)
+- Change terminal directory to vagrant directory inside unzipped directory
+- Start the virtual Machine using `vagrant up`
+- Once done run `vagrant ssh` to log into linux environment
+- Download and unzip [news DB sql file](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip) to vagrant folder which is shared between your OS and VM
+- From VM run `psql -d news -f newsdata.sql` to load DB data
+
+## Usage
+
+In VM terminal run the Log Analyzer to see the output using python or python3:
+`python3 analyzer.py`
+
+## License
+
+The content of this repository is licensed under a [Creative Commons Attribution License](http://creativecommons.org/licenses/by/3.0/us/)
---
+++ analyzer.py
@@ -0,0 +1,105 @@
+# Log Analysis Project
+
+import datetime
+import psycopg2
+
+
+def print_most_popular_articles():
+    '''
+    This function answers below question:
+    1. What are the most popular three articles of all time?
+    '''
+    db = psycopg2.connect(database='news')
+    c = db.cursor()
+    SQL = '''
+        SELECT a.title, COUNT(*) AS views
+        FROM log JOIN
+            (SELECT title, ('/article/' || slug) AS article_slug
+                FROM articles) AS a
+        ON log.path = a.article_slug
+        GROUP BY a.title
+        ORDER BY views DESC
+        LIMIT 3;
+        '''
+    c.execute(SQL)
+    result = c.fetchall()
+    for r in result:
+        print('"{}" — {} views'.format(r[0], r[1]))
+    db.close()
+
+
+def print_most_popular_authors():
+    '''
+    This function answers below question:
+    2. Who are the most popular article authors of all time?
+    '''
+    db = psycopg2.connect(database='news')
+    c = db.cursor()
+    SQL = '''
+        SELECT a.name, COUNT(*) AS views
+        FROM log JOIN
+            (SELECT title, name, ('/article/' || slug) AS article_slug
+                FROM articles JOIN authors
+                ON articles.author = authors.id) AS a
+        ON log.path = a.article_slug
+        GROUP BY a.name
+        ORDER BY views DESC;
+        '''
+    c.execute(SQL)
+    result = c.fetchall()
+    for r in result:
+        print('{} — {} views'.format(r[0], r[1]))
+    db.close()
+
+
+def print_days_of_most_errors():
+    '''
+    This function answers below question:
+    3. On which days did more than 1% of requests lead to errors?
+    '''
+    db = psycopg2.connect(database='news')
+    c = db.cursor()
+    SQL = '''
+        SELECT e.day, (100*e.errors/r.requests) AS errors_percent
+        FROM (SELECT date(time) AS day, COUNT(*) AS errors
+                FROM log
+                WHERE status = '404 NOT FOUND'
+                GROUP BY day) AS e
+            JOIN
+            (SELECT date(time) AS day, COUNT(*) AS requests
+                FROM log
+                GROUP BY day) AS r
+        ON e.day = r.day
+        WHERE (100*e.errors/r.requests) > 1
+        ORDER BY e.errors DESC;
+        '''
+    c.execute(SQL)
+    result = c.fetchall()
+    for r in result:
+        d = datetime.date.strftime(r[0], '%b %d, %Y')
+        print('{} — {}% errors'.format(d, r[1]))
+    db.close()
+
+# Output the analysis
+print('************************************')
+print('**** Log Analyzer for news blog ****')
+print('************************************\n')
+
+print('The most popular three articles of all time:')
+print('--------------------------------------------')
+print_most_popular_articles()
+print('\n')
+
+print('The most popular article authors of all time:')
+print('---------------------------------------------')
+print_most_popular_authors()
+print('\n')
+
+print('The days with more than 1% of requests lead to errors:')
+print('-------------------------------------------------------')
+print_days_of_most_errors()
+print('\n')
+
+print('************************************')
+print('**** End of Log Analyzer output ****')
+print('************************************')
